172b5d11dd3d0485fe5c11239070fec6bc9342ae
Ciilu <109708109+Ciilu@users.noreply.github.com>
2025-07-06 16:39:41 +0800

    GTM

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,6 +1,7 @@
 import {defineUserConfig} from "vuepress";
 import theme from "./theme.js";
 import {getDirname, path} from "vuepress/utils";
+import {googleTagManagerPlugin} from "@vuepress/plugin-google-tag-manager";
 
 const __dirname = getDirname(import.meta.url);
 
@@ -12,6 +13,11 @@ export default defineUserConfig({
     description: "杂七杂八的东西",
     clientConfigFile: path.resolve(__dirname, './client.ts'),
     theme,
+    plugins: [
+        googleTagManagerPlugin({
+            id: 'GTM-5ZFGDSTZ',
+        }),
+    ],
     alias: {
         "@JavaComponent": path.resolve(__dirname, "components/Java.vue"),
         "@OptifineComponent": path.resolve(__dirname, "components/Optifine.vue"),
